4a4020e1cf0321283f528db330b4955107b54a03
Yury Smolsky <yury@smolsky.by>
2022-01-10 19:21:18 +0200

    32: break hmac-sha1 with 3ms delay

---
+++ ch32/main.go
@@ -0,0 +1,198 @@
+package main
+
+import (
+	"crypto/sha1"
+	"encoding/hex"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"os"
+	"time"
+	"tools"
+)
+
+var (
+	key   []byte
+	cache map[string][]byte
+)
+
+const endpoint = "127.0.0.1:8080"
+
+func init() {
+	key = []byte("NDUEj92nd84bwjd")
+	cache = make(map[string][]byte)
+}
+
+func hmacFile(filename string) ([]byte, error) {
+	if sign, ok := cache[filename]; ok {
+		return sign, nil
+	}
+	ws, err := ioutil.ReadFile(filename)
+	if err != nil {
+		return nil, err
+	}
+	sign := hmac(key, ws)
+	cache[filename] = sign
+	return sign, nil
+}
+
+func hmac(key, msg []byte) []byte {
+	if len(key) > sha1.BlockSize {
+		s := sha1.Sum(key)
+		key = s[:]
+	}
+	if len(key) < sha1.BlockSize {
+		key = append(key, make([]byte, sha1.BlockSize-len(key))...)
+	}
+
+	outer := tools.XorBytes(key, []byte{0x5c})
+	inner := tools.XorBytes(key, []byte{0x36})
+	h := sha1.New()
+	h.Write(inner)
+	h.Write(msg)
+	innerSum := h.Sum(nil)
+	h.Reset()
+	h.Write(outer)
+	h.Write(innerSum)
+	return h.Sum(nil)
+}
+
+func server() {
+	http.HandleFunc("/test", func(w http.ResponseWriter, r *http.Request) {
+		r.ParseForm()
+		if !r.Form.Has("file") || !r.Form.Has("sign") {
+			http.Error(w, "bad params", http.StatusBadRequest)
+			return
+		}
+		sign := r.Form.Get("sign")
+		signBytes, err := hex.DecodeString(sign)
+		if err != nil {
+			http.Error(w, "bad params", http.StatusBadRequest)
+			return
+		}
+		file := r.Form.Get("file")
+		expSignature, err := hmacFile(file)
+		if err != nil {
+			http.Error(w, "bad params", http.StatusBadRequest)
+			return
+		}
+		if !insecureEqual(expSignature, signBytes) {
+			http.Error(w, "error", http.StatusInternalServerError)
+			return
+		}
+	})
+	log.Fatal(http.ListenAndServe(endpoint, nil))
+}
+
+func insecureEqual(a, b []byte) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+		time.Sleep(3 * time.Millisecond)
+	}
+	return true
+}
+
+func main() {
+	// should be equal to de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9
+	// fmt.Printf("hmac = %x\n", hmac([]byte("key"), []byte("The quick brown fox jumps over the lazy dog")))
+	if len(os.Args) > 1 && os.Args[1] == "server" {
+		server()
+	} else {
+		breakHmac()
+	}
+}
+
+// sign[6] = 230 el = 162
+// sign[7] = 172 el = 185
+// sign[8] = 21 el = 208
+// sign[9] = 156 el = 235
+// sign[10] = 251 el = 252
+// sign[11] = 92 el = 278
+
+func breakHmac() {
+	url := fmt.Sprintf("http://%s/test?file=wordlist.txt&sign=", endpoint)
+	sign := make([]byte, 20)
+	const startPos = 0
+	const bytesDelay = 3000
+	const nextDelay = bytesDelay - 300
+	const callsInit = 100
+	const calls = 10
+	// make calls to average the response time for wrong bytes
+	var avg int64
+	for b := 0; b < callsInit; b++ {
+		sign[startPos] = byte(b)
+		t := respTime(url, sign)
+		fmt.Printf("%d ", t)
+		avg += t
+	}
+	avg /= callsInit
+	exp := make([]int64, len(sign))
+	exp[startPos] = avg + nextDelay
+
+	fmt.Println("\navg=", avg, "next >", exp)
+	for i := startPos; i < len(sign); i++ {
+		sign[i] = 0
+		fails := 0
+		for {
+			time.Sleep(2 * time.Millisecond)
+			elapsed := respTime(url, sign)
+			// fmt.Printf("%d ", elapsed)
+			if elapsed >= exp[i] {
+				fmt.Printf("cand ")
+				var avg int64
+				for b := 0; b < calls; b++ {
+					time.Sleep(2 * time.Millisecond)
+					t := respTime(url, sign)
+					fmt.Printf("%d ", t)
+					avg += t
+				}
+				avg /= calls
+				fmt.Println("")
+				if avg >= exp[i] {
+					if i+1 < len(sign) {
+						exp[i+1] = avg + nextDelay
+					}
+					fmt.Printf("sign[%d] = 0x%x t = %d next > %d\n", i, sign[i], avg, exp)
+					break
+				}
+			}
+			sign[i]++
+			if sign[i] == 0 {
+				// we have not found any sign[i] that produces higher then
+				// average response time.
+				fails++
+				fmt.Println("failed times", fails)
+				if fails >= 5 {
+					// if too many fails that let's retry previous byte
+					fmt.Println("retrying prev byte")
+					sign[i] = 0
+					i--
+					sign[i] = 0
+					fails = 0
+				}
+			}
+		}
+	}
+	fmt.Println(hex.EncodeToString(sign))
+}
+
+func respTime(url string, sign []byte) int64 {
+	url = url + hex.EncodeToString(sign)
+	timer := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer resp.Body.Close()
+	// _, err = io.ReadAll(resp.Body)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	return time.Since(timer).Microseconds()
+}
